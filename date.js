fe082d4eb7509b6c5e650feaf7cd4d2ecd4ce80e
nithish125 <nithishs3620@gmail.com>
2022-07-14 18:18:45 +0530

    modified.

--- date.js
+++ date.js
@@ -1,64 +1,56 @@
-var selectyear=document.getElementById("year")
-var selectmonth=document.getElementById("month")
-var selectdate=document.getElementById("date")
+var selectyear = document.getElementById("year")
+var selectmonth = document.getElementById("month")
+var selectdate = document.getElementById("date")
 console.log(selectmonth)
 
 
 //console.log( selectdate)
-function findmonth(){
-     var m=selectmonth.value
-     console.log(m)
-     
-     
- if( m==2){
-    
-    if(selectyear.value%4==0){
-        selectdate.innerHTML=''
-        for(i=1;i<=29;i++){
-            var option = document.createElement("option");
-            option.innerText=i
-            selectdate.add(option)
-            
-            
+function findmonth() {
+    var m = selectmonth.value
+    console.log(m)
+
+
+    if (m == 2) {
+
+        if (selectyear.value % 4 == 0) {
+            selectdate.innerHTML = ''
+            for (i = 1; i <= 29; i++) {
+                var option = document.createElement("option");
+                option.innerText = i
+                selectdate.add(option)
+
+
+            }
+
         }
-        
+        else {
+            selectdate.innerHTML = ''
+            for (i = 1; i <= 28; i++) {
+                var option = document.createElement("option");
+                option.innerText = i
+                selectdate.add(option)
+
+            }
+        }
+
     }
-    else{
-        selectdate.innerHTML=''
-        for(i=1;i<=28;i++){
+    else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+        selectdate.innerHTML = ''
+        for (i = 1; i <= 31; i++) {
             var option = document.createElement("option");
-            option.innerText=i
+            option.innerText = i
             selectdate.add(option)
-            
         }
     }
-    
- }
- else if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-    selectdate.innerHTML=''
-    for(i=1;i<=31;i++){
-        var option = document.createElement("option");
-            option.innerText=i
+    else {
+        selectdate.innerHTML = ''
+        for (i = 1; i <= 30; i++) {
+            var option = document.createElement("option");
+            option.innerText = i
             selectdate.add(option)
+
+        }
     }
- }
- else{
-    selectdate.innerHTML=''
-    for(i=1;i<=30;i++){
-        var option = document.createElement("option");
-            option.innerText=i
-            selectdate.add(option)
-        
-    }  
- }
- console.log(selectyear.value)
-} 
-//  var x=function myF(k) {
-    
-//     var option = document.createElement("option");
-//     option.value = "k";
-//     console.log(option.value);
-//     option.text = "k";
-//     selectdate.add(option);
-//   }
+    console.log(selectyear.value)
+}
 
