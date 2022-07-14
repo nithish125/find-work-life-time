d3a068549fb3414860428f2a2765babbaa702a25
nithish125 <nithishs3620@gmail.com>
2022-07-14 16:50:53 +0530

    modified.

---
+++ date.js
@@ -0,0 +1,60 @@
+var selectyear=document.getElementById("year")
+var selectmonth=document.getElementById("month")
+var selectdate=document.getElementById("date")
+console.log(selectmonth)
+
+
+//console.log( selectdate)
+function findmonth(){
+     var m=selectmonth.value
+     console.log(m)
+     
+     
+ if( m==2){
+    
+    if(selectyear.value%4==0){
+        for(i=1;i<=29;i++){
+            var option = document.createElement("option");
+            option.innerText=i
+            selectdate.add(option)
+            
+            
+        }
+        
+    }
+    else{
+        for(i=1;i<=28;i++){
+            var option = document.createElement("option");
+            option.innerText=i
+            selectdate.add(option)
+            
+        }
+    }
+    
+ }
+ else if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+    for(i=1;i<=31;i++){
+        var option = document.createElement("option");
+            option.innerText=i
+            selectdate.add(option)
+    }
+ }
+ else{
+    for(i=1;i<=30;i++){
+        var option = document.createElement("option");
+            option.innerText=i
+            selectdate.add(option)
+        
+    }  
+ }
+ console.log(selectyear.value)
+} 
+//  var x=function myF(k) {
+    
+//     var option = document.createElement("option");
+//     option.value = "k";
+//     console.log(option.value);
+//     option.text = "k";
+//     selectdate.add(option);
+//   }
+
